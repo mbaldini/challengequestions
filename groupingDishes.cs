9cbea73e15665b06eadb29cec4a3a3de43435f0b
Mike Baldini <mbaldini@live.com>
2020-02-28 10:13:56 -0800

    Update groupingDishes.cs

--- groupingDishes.cs
+++ groupingDishes.cs
@@ -1,6 +1,11 @@
-// You have a list of dishes. Each dish is associated with a list of ingredients used to prepare it. You want to group the dishes by ingredients, so that for each ingredient you'll be able to find all the dishes that contain it (if there are at least 2 such dishes).
+// You have a list of dishes. Each dish is associated with a list of ingredients used to prepare it. 
+// You want to group the dishes by ingredients, so that for each ingredient you'll be able to find all 
+// the dishes that contain it (if there are at least 2 such dishes).
 
-// Return an array where each element is a list with the first element equal to the name of the ingredient and all of the other elements equal to the names of dishes that contain this ingredient. The dishes inside each list should be sorted lexicographically. The result array should be sorted lexicographically by the names of the ingredients in its elements.
+// Return an array where each element is a list with the first element equal to the name of the ingredient 
+// and all of the other elements equal to the names of dishes that contain this ingredient. The dishes 
+// inside each list should be sorted lexicographically. The result array should be sorted lexicographically 
+// by the names of the ingredients in its elements.
 
 // Example
 
@@ -32,7 +37,10 @@
 
 // [input] array.array.string dishes
 
-// An array of dishes. dishes[i] for each valid i contains information about the ith dish: the first element of dishes[i] is the name of the dish and the following elements are the ingredients of that dish. Both the dish name and the ingredient names consist of English letters and spaces. It is guaranteed that all dish names are different. It is also guaranteed that ingredient names for one dish are also pairwise different.
+// An array of dishes. dishes[i] for each valid i contains information about the ith dish: the first element 
+// of dishes[i] is the name of the dish and the following elements are the ingredients of that dish. Both the
+// dish name and the ingredient names consist of English letters and spaces. It is guaranteed that all dish 
+// names are different. It is also guaranteed that ingredient names for one dish are also pairwise different.
 
 // Guaranteed constraints:
 // 1 ≤ dishes.length ≤ 500,
